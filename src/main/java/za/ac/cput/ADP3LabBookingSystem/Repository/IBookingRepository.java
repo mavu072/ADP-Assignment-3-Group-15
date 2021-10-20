62708ec1c3452c6c3b28a69cf36a05ace8159327
Avuyile Mgxotshwa <78750424+mavu072@users.noreply.github.com>
2021-10-20 20:12:37 +0200

    Update IBookingRepository.java

--- src/main/java/za/ac/cput/ADP3LabBookingSystem/Repository/IBookingRepository.java
+++ src/main/java/za/ac/cput/ADP3LabBookingSystem/Repository/IBookingRepository.java
@@ -5,7 +5,7 @@ import za.ac.cput.ADP3LabBookingSystem.Entity.Booking;
 import za.ac.cput.ADP3LabBookingSystem.Repository.IRepository;
 import java.util.Set;
 
-public interface IBookingRepository extends IRepository<Booking, String> {
+public interface IBookingRepository extends JpaRepository<Booking, String> {
 
     public Set<Booking> getAll();
 }
